bb271abb87d37ebc45d2edde327eca305c30860e
Liam Ross <liamross.code@gmail.com>
2018-05-01 01:02:53 -0700

    Updated index to export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+// Paper exports.
+export { IPaperProps, IPaperInputNode, IPaperInputEdge } from './Paper/IPaper';
 export { Paper } from './Paper/Paper';
+
+// Node exports.
+export { INodeProps } from './Node/INode';
 export { Node } from './Node/Node';
+
+// Edge exports.
+export { IEdgeProps } from './Edge/IEdge';
+// export { Edge } from './Edge/Edge';
